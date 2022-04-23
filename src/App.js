c7ff338b4cd835a45b250528aabaa4876ceec702
Sourabh8037 <hande.sourabh@gmail.com>
2022-04-23 16:42:22 +0530

    v1.0.1.2

--- src/App.js
+++ src/App.js
@@ -1,73 +1,151 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Box, Button, Grid, IconButton, Modal, TextField, Typography } from '@mui/material';
+import * as React from 'react'
+import {Alert, Button, ButtonGroup, Grid, IconButton, Modal, Snackbar, TextField, Typography } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { gradientStyles, modal1, modal2 } from './constant';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 
-const gradientStyles = [
-  {backgroundImage: "linear-gradient(to right, #ff512f, #f09819)"},
-  {backgroundImage: "linear-gradient(to right, #00b09b, #96c93d"},
-  {backgroundImage: "radial-gradient( circle farthest-corner at 22.4% 21.7%, rgba(4,189,228,1) 0%, rgba(2,83,185,1) 100.2% )"},
-  {backgroundImage: "linear-gradient(to right, #8e2de2, #4a00e0)"},
-]
+// const Alert = React.forwardRef<HTMLDivElement,AlertProps>(function Alert(
+//   props,
+//   ref,
+// ) {
+//   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+// });
+
 
 function App() {
-  const [list,setList] = useState([]);
-  const [showModal,setShowModal] = useState(false)
+  const [state,setState] = useState({
+    collection:{},
+    currList:[]
+  })
+  const [showCollection,setShowCollection] = useState(false)
+  const [showListModal,setShowListModal] = useState(false)
+  const [showCollectionModal,setShowCollectionModal] = useState(false)
   const [modalText,setModalText] = useState('')  
   const [prevText,setPrevText] = useState('')
+  const [currentCollectionKey, setCurrentCollectionKey] = useState('')
+  const [message,setMessage] = useState(null)
 
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem('store'));
     if (items) {
-     setList(items);
+     setState(items);
     }
   }, []);
   
   const handleClose = ()=>{
     setModalText('')
     if(prevText!=='')setPrevText('')
-    setShowModal(false)
+    setShowListModal(false)
+    setShowCollectionModal(false)
   }
   
-  const handleOpen = (isEdit,prev)=>{
+  const handleOpen = (isEdit,prev,isModal1)=>{
+    if(isModal1){
     if(isEdit){
       setModalText(prev)
       setPrevText(prev)
     }
     console.log(prev);
-    setShowModal(true)
+    setShowListModal(true)
+  }
+  else{
+    setShowCollectionModal(true)
+  }
   }
   
   const textChangeHandler = (e) =>{
     setModalText(e.target.value)
   }
 
+  // LIST METHODS
   const addTask = (e)=>{
     e.preventDefault()
     if(modalText===''){
-      handleClose()
+      showMessage('TaskName is Compulsory. Try Again!','error')
       return
     }
     if(prevText!==''){
-      const newList = list.map(item=>(item===prevText)?modalText:item)
-      setList(newList)
+      const newList = state.currList.map(item=>(item===prevText)?modalText:item)
+      setState(st=>({...state,currList:newList}))
       setPrevText('')
     } 
     else{
-      setList(l=>[...l,modalText])
+      setState(st=>({...state,currList:[...st.currList,modalText]}))
     }
     handleClose();
   }
   
   const removeTask = (item) =>{
-    const newList = list.slice()
+    const newList = state.currList.slice()
     newList.splice(newList.indexOf(item),1)
-    setList(newList)
+    setState(st=>({...state,currList:newList}))
   }
 
-  const saveList = () =>localStorage.setItem('store',JSON.stringify(list))  
-  const resetList = () =>setList([])
+  const saveList = () =>{
+    if(currentCollectionKey!==''){
+    setState(st=>({collection:{...st.collection,[currentCollectionKey]:st.currList}, currList:[]}))
+    setCurrentCollectionKey('')
+    }
+    localStorage.setItem('store',JSON.stringify(state))
+    showMessage('Data Saved','info')
+  }
+  const resetList = () =>{
+    const items = JSON.parse(localStorage.getItem('store'));
+    if (items) {
+     setState(items);
+    }else{
+      setState({
+        collection:{},
+        currList:[]
+      });
+    }
+  }
+  const toggleShowCollection = () =>setShowCollection(b=>!b)
+  
+  // MESSAGE METHODS
+  const showMessage = (msg,severity) =>{
+    setMessage({msg,severity})
+    setTimeout(() => {
+      setMessage('')
+    }, 3000);
+  }
+  const closeMessage = () => setMessage(null)
+
+
+  // COLLECTION METHODS
+  const addToCollection = (e) =>{
+    e.preventDefault()
+    if(modalText===''){
+      showMessage('Title is compulsary. Try again!','error')
+      return
+    }
+    const newCollection = JSON.parse(JSON.stringify(state.collection))
+    newCollection[modalText] = state.currList.slice()
+    setState(st=>({...st,collection:newCollection}))
+    handleClose()
+    showMessage('Added To Collection','success')
+  }
+
+  const removeFromCollection = (key)=>{
+    const newCollection = JSON.parse(JSON.stringify(state.collection))
+    delete newCollection[key]
+    setState(st=>({...st,collection:newCollection}))
+  }
+
+  const editCollectionList = (key)=>{
+    setState(st=>({...st,currList:[...st.collection[key]]}))
+    setCurrentCollectionKey(key)
+    setShowCollection(false)
+  }
+
+  const takeToCurrList = (key)=>{
+    setState(st=>({...st,currList:[...st.collection[key]]}))
+    setCurrentCollectionKey('')
+    setShowCollection(false)
+  }
 
   return (
     <div className="App">
@@ -77,23 +155,28 @@ function App() {
             <Typography className='clrwhite' variant='h4' fontWeight={"400"}>What would you like to do today?</Typography>
           </Grid>
           <Grid item xs={5}>
-            <button className="btn" style={gradientStyles[0]} onClick={handleOpen.bind(this,false,-1)}>Add Todo</button>
+            <button className="btn" style={gradientStyles[0]} onClick={handleOpen.bind(this,false,-1,true)}>Add Todo</button>
           </Grid>
           <Grid item xs={5}>
-            <button className="btn" style={gradientStyles[1]} onClick={saveList}>Save List</button>
+            <button className="btn" style={gradientStyles[1]} onClick={saveList}>Save</button>
           </Grid>
           <Grid item xs={5}>
             <button className="btn" style={gradientStyles[2]} onClick={resetList}>Reset</button>
           </Grid>
-          {/* <Grid item xs={5}>
-            <button className="btn" style={gradientStyles[3]}>Add To Collection</button>
-          </Grid> */}
-          {
-            list.map((item,index)=><Grid
-               key={item} item  xs={11} container justifyContent="space-around" alignItems={'center'} className="task" style={gradientStyles[index%4]}>
+          <Grid item xs={5}>
+            <button className="btn" style={gradientStyles[3]} onClick={handleOpen.bind(this,'','',false)}>Add To Collection</button>
+          </Grid>
+          <Grid item xs={5}>
+            <button className="btn" style={gradientStyles[4]} onClick={toggleShowCollection}>{showCollection?'Go To List':'View Collection'}</button>
+          </Grid>
+
+          {/* LIST VIEW */}
+          {!showCollection &&
+            state.currList.map((item,index)=><Grid
+               key={item} item  xs={11} container justifyContent="space-around" alignItems='center'className="task" style={gradientStyles[index%5]}>
                  <Grid item xs={8}><Typography variant='inherit' align='left'>{item}</Typography></Grid>
                  <Grid item xs={1}>
-                   <IconButton variant="contained" size='large' onClick={handleOpen.bind(this,true,item)}>
+                   <IconButton variant="contained" size='large' onClick={handleOpen.bind(this,true,item,true)}>
                     <EditIcon fontSize='medium'></EditIcon>
                    </IconButton>
                  </Grid>
@@ -104,15 +187,32 @@ function App() {
                  </Grid>
                </Grid>)               
           }
+
+          {/* COLLECTION VIEW */}
+          {showCollection &&
+            Object.keys(state.collection).map((item,index)=><Grid
+               key={item} item  xs={11} container justifyContent="space-around" alignItems={'center'} className="task" style={gradientStyles[index%5]}>
+                 <Grid item xs={6}><Typography variant='inherit' align='left'>{item}</Typography></Grid>
+                 <Grid item xs={6}>
+                 <ButtonGroup variant="filled" aria-label="outlined primary button group">
+                    <Button onClick={editCollectionList.bind(this,item)}>Edit</Button>
+                    <Button onClick={removeFromCollection.bind(this,item)}>Delete</Button>
+                    <Button onClick={takeToCurrList.bind(this,item)}>Select</Button>
+                  </ButtonGroup>
+                 </Grid>
+               </Grid>)               
+          }
         </Grid>        
       </Grid>
+
+      {/* LIST MODAL */}
       <Modal
-        open={showModal}
+        open={showListModal}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <form className='modal' onSubmit={addTask}>
+        <form className='modal' style={{background:modal1}} onSubmit={addTask}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <Typography id="modal-modal-title" className='clrwhite' variant="h4" align='center' component="h2">
@@ -133,6 +233,40 @@ function App() {
           </Grid>
         </form>
       </Modal>
+
+      {/* COLLECTION MODAL */}
+      <Modal
+        open={showCollectionModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <form className='modal' style={{backgroundImage:modal2}} onSubmit={addToCollection}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography id="modal-modal-title" className='clrwhite' variant="h4" align='center' component="h2">
+              Add Title to your List
+            </Typography>
+            </Grid>
+          <Grid item xs={12}>
+          <TextField id="filled-basic" label={<Typography className='clrwhite'>Your task!</Typography>} variant="filled" fullWidth onChange={textChangeHandler} value={modalText} className='clrwhite' InputProps={{inputProps:{className:'clrwhite'}}}/>
+          </Grid>
+          <Grid item xs={12} container justifyContent={'space-around'}>
+            <Grid item xs={5}>
+              <Button type='submit' variant='contained' color='success' onClick={addToCollection} fullWidth>{prevText!==''?'Replace':'Add'}</Button>
+            </Grid>
+            <Grid item xs={5}>
+              <Button type='button' variant='contained' color="error" onClick={handleClose} fullWidth>Cancel</Button>
+            </Grid>
+          </Grid>
+          </Grid>
+        </form>
+      </Modal>
+      {message && <Snackbar anchorOrigin={{horizontal:'center',vertical:'bottom'}} open={showMessage!==null} autoHideDuration={3000} onClose={closeMessage}>
+        <MuiAlert variant='filled' elevation={8} onClose={closeMessage} severity={message?.severity} sx={{ width: '100%' }}>
+          {message?.msg}
+        </MuiAlert>
+      </Snackbar>}
     </div>
   );
 }
